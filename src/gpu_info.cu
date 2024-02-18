75024392b6906ede7686a3ea42e3d7893bc2f811
LBF38 <contact.lbf38@gmail.com>
2024-02-18 19:25:53 +0100

    feat(cuda): :sparkles: add gpu_info program

---
+++ src/gpu_info.cu
@@ -0,0 +1,32 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+
+int main() {
+  int deviceCount;
+  cudaGetDeviceCount(&deviceCount);
+
+  if (deviceCount == 0) {
+    printf("No CUDA-capable devices detected\n");
+    return 0;
+  }
+
+  printf("Number of CUDA devices: %d\n", deviceCount);
+
+  for (int i = 0; i < deviceCount; ++i) {
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp, i);
+
+    printf("\nDevice %d:\n", i);
+    printf("  Device name: %s\n", deviceProp.name);
+    printf("  Compute capability: %d.%d\n", deviceProp.major, deviceProp.minor);
+    printf("  Total global memory: %.2f GB\n",
+           static_cast<float>(deviceProp.totalGlobalMem) /
+               (1 << 30)); // 1GB = 2^30 bytes = 1024^3 bytes
+    printf("  Number of multiprocessors: %d\n", deviceProp.multiProcessorCount);
+    printf("  Maximum threads per block: %d\n", deviceProp.maxThreadsPerBlock);
+    printf("  Maximum threads per multiprocessor: %d\n",
+           deviceProp.maxThreadsPerMultiProcessor);
+  }
+
+  return 0;
+}
